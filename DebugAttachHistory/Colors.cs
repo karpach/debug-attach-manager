13ee929e8ef91f1d9e9d85c6c69af29aa3dc83b3
Viktar Karpach <karpach@gmail.com>
2015-09-18 21:37:12 +0000

    Partial conversion to theme support

---
+++ DebugAttachHistory/Colors.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+
+namespace Karpach.DebugAttachManager
+{
+    public class Colors
+    {
+        public static ThemeResourceKey ToobarBackgroundBegin=> EnvironmentColors.CommandBarGradientBeginColorKey;
+        public static ThemeResourceKey ToobarBackgroundEnd => EnvironmentColors.CommandBarGradientEndColorKey;
+        public static ThemeResourceKey MainBackground => EnvironmentColors.ToolWindowBackgroundBrushKey;
+        public static ThemeResourceKey MainForeground => EnvironmentColors.ToolWindowTextBrushKey;
+        public static ThemeResourceKey SearchBackground => EnvironmentColors.SearchBoxBackgroundBrushKey;
+        public static ThemeResourceKey SearchBorder => EnvironmentColors.SearchBoxBorderBrushKey;
+        public static ThemeResourceKey SearchPlaceHolderForeground => EnvironmentColors.ControlEditHintTextBrushKey;
+        public static ThemeResourceKey LinkForeground => EnvironmentColors.ControlLinkTextBrushKey;
+        public static ThemeResourceKey LinkHoverForeground => EnvironmentColors.MainWindowActiveIconDebuggingBrushKey;
+    }
+}
