afdbc73e75baed8f065e55632431c9e49efdf109
Viktar Karpach <viktar.karpach@relativity.com>
2021-05-06 10:08:26 -0500

    Mask password field

--- DebugAttachHistory/Properties/AssemblyInfo.cs
+++ DebugAttachHistory/Properties/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.2.4261.0")]
-[assembly: AssemblyFileVersion("2.2.4261.0")]
+[assembly: AssemblyVersion("2.3.5051.0")]
+[assembly: AssemblyFileVersion("2.3.5051.0")]
 
 
 
